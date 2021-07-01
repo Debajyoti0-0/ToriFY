22d7fd225825d3e0d3f08b0aa20c98c70547d7b1
Debajyoti Haldar <79437050+Debajyoti0-0@users.noreply.github.com>
2021-07-01 14:49:32 +0530

    Add files via upload

---
+++ colorizor.py
@@ -0,0 +1,28 @@
+
+class COLOR:
+    GREEN = '\033[32m'
+    LIGHT_GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    DARK_YELLOW = '\33[33m'
+    RED = '\033[31m'
+    LIGHT_RED = '\033[91m'
+    PURPLE = '\033[35m'
+    PINK = '\033[95m'
+    BLUE = '\033[34m'
+    BRIGHT_BLUE = '\033[94m'
+    CYAN = '\033[36m'
+    LIGHT_CYAN = '\033[96m'
+    WHITE = '\033[0m'
+    LIGHT_GRAY = '\033[37m'
+    DARK_GRAY = '\033[90m'
+    VIOLET = '\33[35m'
+    ORANGE = '\033[33m'
+    REST = '\033[0m'
+
+class STYLE:
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ITALIC = '\033[3m'
+
+
+
