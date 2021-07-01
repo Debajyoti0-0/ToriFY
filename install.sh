dbc39ee7d4852dba50ceea5cd16edc2b70519130
Debajyoti Haldar <79437050+Debajyoti0-0@users.noreply.github.com>
2021-07-01 14:47:33 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#git clone https://github.com/Und3rf10w/kali-anonsurf
+sudo apt-get install macchanger;sudo dnf install macchanger;sudo pacman -S macchanger
+cd kali-anonsurf && chmod +x * && sudo ./installer.sh
