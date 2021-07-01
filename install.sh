d3eada9a4b65259584e3dc5901649f9df91e808c
Debajyoti Haldar <79437050+Debajyoti0-0@users.noreply.github.com>
2021-07-01 16:58:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #git clone https://github.com/Und3rf10w/kali-anonsurf
 sudo apt-get install macchanger;sudo dnf install macchanger;sudo pacman -S macchanger
+sudo apt-get install tor;sudo dnf install tor;sudo pacman -S tor
 cd kali-anonsurf && chmod +x * && sudo ./installer.sh
