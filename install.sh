b6cd12ca9270e61d0d31486035fc0f934a478f6b
Debajyoti Haldar <79437050+Debajyoti0-0@users.noreply.github.com>
2024-07-03 11:39:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
-#git clone https://github.com/Und3rf10w/kali-anonsurf
-sudo apt-get install macchanger;sudo dnf install macchanger;sudo pacman -S macchanger
-sudo apt-get install tor;sudo dnf install tor;sudo pacman -S tor
-cd kali-anonsurf && chmod +x * && sudo ./installer.sh
+ 
+# Function to install packages based on the package manager
+install_package() {
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get install -y "$1"
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y "$1"
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -S --noconfirm "$1"
+    else
+        echo "\n[!] Unsupported package manager. Please install $1 manually."
+        exit 1
+    fi
+}
+ 
+# Check and install macchanger
+clear
+echo "[+] Installing macchanger..."
+install_package macchanger
+ 
+# Check and install tor
+clear
+echo "[+] Installing tor..."
+install_package tor
+ 
+# Change directory and install kali-anonsurf
+if [ -d "kali-anonsurf" ]; then
+    cd kali-anonsurf
+    chmod +x *
+    sudo ./installer.sh
+else
+    echo "[!] Directory kali-anonsurf not found"
+    exit 1
+fi
+ 
+clear
+echo "[*] Installation completed."
