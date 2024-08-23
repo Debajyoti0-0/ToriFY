7ec7100c971c8e6e6f0b5dde9ee5306adae03b1f
Debajyoti Haldar <79437050+Debajyoti0-0@users.noreply.github.com>
2024-08-23 15:51:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
- 
+
 # Function to install packages based on the package manager
 install_package() {
-    if command -v apt-get &> /dev/null; then
-        sudo apt-get install -y "$1"
-    elif command -v dnf &> /dev/null; then
-        sudo dnf install -y "$1"
-    elif command -v pacman &> /dev/null; then
-        sudo pacman -S --noconfirm "$1"
-    else
-        echo "\n[!] Unsupported package manager. Please install $1 manually."
-        exit 1
-    fi
+    if command -v apt-get &> /dev/null; then
+        sudo apt-get install -y "$1"
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y "$1"
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -S --noconfirm "$1"
+    else
+        echo -e "\n[!] Unsupported package manager. Please install $1 manually."
+        exit 1
+    fi
 }
- 
+
 # Check and install macchanger
 clear
 echo "[+] Installing macchanger..."
 install_package macchanger
- 
+
 # Check and install tor
 clear
 echo "[+] Installing tor..."
 install_package tor
- 
+
 # Change directory and install kali-anonsurf
 if [ -d "kali-anonsurf" ]; then
-    cd kali-anonsurf
-    chmod +x *
-    sudo ./installer.sh
+    cd kali-anonsurf
+    chmod +x *
+    sudo ./installer.sh
 else
-    echo "[!] Directory kali-anonsurf not found"
-    exit 1
+    echo "[!] Directory kali-anonsurf not found"
+    exit 1
 fi
- 
+
 clear
 echo "[*] Installation completed."
